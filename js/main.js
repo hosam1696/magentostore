60f70a9340900762fb060cd04992954c32435731
HosamElnabawy <hosamelnabawy1996@gmail.com>
2021-02-02 20:43:36 +0200

    Add Floating Button to scroll to top page

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
-$(function() {
-   
-    $(window).on('scroll', (e) =>{
+$(function () {
+    const floatingBtn = $('.floating-btn');
+    let windowHeight = $(window).height();
+
+    $(window).on('scroll', (e) => {
         console.log(e);
-    } )
+        if (windowHeight < $(window).scrollTop()) {
+            floatingBtn.removeClass('hidden')
+        } else {
+            if (!floatingBtn.hasClass('hidden')) {
+                floatingBtn.addClass('hidden')
+            }
+        }
+
+
+    });
+
+    floatingBtn.on('click', (e) => {
+        $("html, body").animate({scrollTop: 0}, 500);
+    })
 });
